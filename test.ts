614af34943401748a99ca7dd938eb5a98b2e9610
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-10-11 17:41:29 +0200

    Installed TS package for node and added comments as to why this is installed

--- test.ts
+++ test.ts
@@ -1,3 +1,11 @@
+// TypeScript expects all globally used variables to be 'typed' as in, complying to TS rules
+// The DefinitelyTyped community are maintaining most of the npm packages to also have a typed variant (React, node etc.)
+// For example, the process global variable from Node (process.argv etc.) is not accessible without installing said npm ts package
+
+// You can install them by prefixing it with @types/*insert package name here*
+// Always save these to dev dependecies since TS is only before compilation, production will run the build made from said TS files aka using regular compiled JS
+
+
 // The type keyword allows you to make a type 'alias' aka giving your type a name
 // A union type is used when a type has values that can overlap
 type Operation = 'add' | 'multiply' | 'divide'
